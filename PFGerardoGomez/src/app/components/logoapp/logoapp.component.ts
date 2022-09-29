11d72a45e89a1da766330bf798eff0f9b322a414
gerag84 <96827421+gerag84@users.noreply.github.com>
2022-09-29 16:07:55 -0300

    primer commit

---
+++ PFGerardoGomez/src/app/components/logoapp/logoapp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logoapp',
+  templateUrl: './logoapp.component.html',
+  styleUrls: ['./logoapp.component.css']
+})
+export class LogoappComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
